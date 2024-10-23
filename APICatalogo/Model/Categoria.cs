380c1b67ed2b6ca0d6b3e236aa1934cca62a5920
Jocemar Hoppe Pereira <jocemarpere@gmail.com>
2024-10-22 22:31:30 -0300

    Criação do projeto com as classes do projeto

---
+++ APICatalogo/Model/Categoria.cs
@@ -0,0 +1,8 @@
+﻿namespace APICatalogo.Model;
+
+public class Categoria
+{
+    public int CategoriaId { get; set; }
+    public string? Nome { get; set; }
+    public string? ImagemUrl { get; set; }
+}
